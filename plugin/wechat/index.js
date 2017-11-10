e04062496d54449620b5017e0f3823601d9327e8
蔡荣庚 <89987112@qq.com>
2017-11-10 10:17:29 +0800

    v0.1

--- plugin/wechat/index.js
+++ plugin/wechat/index.js
@@ -5,27 +5,33 @@ const isDebug = false;
 export default {
   install(Vue){
     Vue.prototype.$config = function (jsApis) {
+     return new Promise((resolve,reject)=>{
+       //主要是找不到wx变量报错
+       try{
+         if(wx){
+           jsApis = [].concat(jsApis);
 
-      try{
-        if(wx){
-          jsApis = [].concat(jsApis);
+           wechat.sign(window.location.href).then((data)=>{
+             data = data.data;
+             wx.config({
+               debug: isDebug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+               appId: data.appId, // 必填，公众号的唯一标识
+               timestamp:data.timestamp , // 必填，生成签名的时间戳
+               nonceStr: data.nonceStr, // 必填，生成签名的随机串
+               signature:data.signature,// 必填，签名，见附录1
+               jsApiList: jsApis // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+             });
+           });
 
-          wechat.sign(window.location.href).then((data)=>{
-            data = data.data;
-            wx.config({
-              debug: isDebug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-              appId: data.appId, // 必填，公众号的唯一标识
-              timestamp:data.timestamp , // 必填，生成签名的时间戳
-              nonceStr: data.nonceStr, // 必填，生成签名的随机串
-              signature:data.signature,// 必填，签名，见附录1
-              jsApiList: jsApis // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-            });
-          });
+           wx.ready(function () {
+             resolve();
+           })
+         }
+       }catch (e){
+         console.log(e);
+       }
+     })
 
-        }
-      }catch (e){
-        console.log(e);
-      }
 
 
     }
--- wechat/menus.js
+++ wechat/menus.js
@@ -1,8 +1,8 @@
-const BASE_URL = "http://192.168.1.240:8080";
+const BASE_URL = "http://192.168.1.240:80";
 
 
-// 先根据 https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240:8080/ ....... 的格式跳转到后台,然后后台重定向到我的开发电脑上
-//
+// 先根据 https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240/ ....... 的格式跳转到后台,然后后台重定向到我的开发电脑上
+
 
 module.exports ={
   "button":[
@@ -12,18 +12,18 @@ module.exports ={
         {
           "type": "view",
           "name": "申报安装",
-          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/declareInstall&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
-          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240:8080/wechatRedirect/declareInstall&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
+          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/926274937821114368/declareInstall&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
+          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240/wechatRedirect/926274937821114368/declareInstall&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
         },{
           "type": "view",
           "name": "申报维修",
-          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/declareRepair&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
-          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240:8080/wechatRedirect/declareRepair&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
+          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/926274937821114368/declareRepair&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
+          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240/wechatRedirect/926274937821114368/declareRepair&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
         },{
           "type": "view",
           "name": "服务记录",
-          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/serviceRecord&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
-          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240:8080/wechatRedirect/serviceRecord&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
+          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/926274937821114368/serviceRecord&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
+          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240/wechatRedirect/926274937821114368/serviceRecord&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
         }
       ]
     },
@@ -33,13 +33,13 @@ module.exports ={
         {
           "type": "view",
           "name": "会员注册",
-          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/memberRegister&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
-          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240:8080/wechatRedirect/memberRegister&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
+          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/926274937821114368/memberRegister&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
+          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240/wechatRedirect/926274937821114368/memberRegister&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect`
         },{
           "type": "view",
           "name": "个人中心",
-          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/personalCenter&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
-          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240:8080/wechatRedirect/personalCenter&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
+          "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=${BASE_URL}/wechatRedirect/926274937821114368/personalCenter&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
+          // "url":`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5dec4882587fb612&redirect_uri=http://192.168.1.240/wechatRedirect/926274937821114368/personalCenter&response_type=code&scope=snsapi_base&state=bind001#wechat_redirect`
         },{
           "type": "view",
           "name": "消费记录",
